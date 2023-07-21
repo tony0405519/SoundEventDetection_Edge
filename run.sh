4893a56cc7eb95f70204781cd9ec314ca0768257
tony0405519 <32356872+tony0405519@users.noreply.github.com>
2023-07-21 15:10:46 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 sudo docker run -it --ipc host \
 --device /dev/video0:/dev/video0 \
+--device /dev/video1:/dev/video1 \
+--device /dev/snd:/dev/snd \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -e DISPLAY=$DISPLAY \
 sound_detect bash
