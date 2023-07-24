6f7c118caf08c5aaf0f2fb0020d2066524f169b7
tony0405519 <32356872+tony0405519@users.noreply.github.com>
2023-07-24 11:38:51 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ sudo docker run -it --ipc host \
 --device /dev/snd:/dev/snd \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -e DISPLAY=$DISPLAY \
+-e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+-v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+-v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+--group-add $(getent group audio | cut -d: -f3) \
 sound_detect bash
